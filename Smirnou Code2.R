4e2d9f893487232b41dd99ef48cc79f113f0b0c3
StassSmir <97925680+StassSmir@users.noreply.github.com>
2022-01-31 01:29:36 -0500

    pre-presidential polls

---
+++ Smirnou Code2.R
@@ -0,0 +1,14 @@
+> Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+> ABC <- c(4, 62, 51, 21, 2, 14, 15)
+> CBS <- c(12, 75, 43, 19, 1, 21, 19)
+> grades <- c(Name, ABC, CBS)
+> grades
+
+
+> Name <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Berine")
+> ABC <- c(4, 62, 51, 21, 2, 14, 15)
+> CBS <- c(12, 75, 43, 19, 1, 21, 19)
+> grades <- cbind(Name, ABC, CBS)
+> grades 
+> grades.df <- data.frame(Name, ABC, CBS)
+> grades.df
