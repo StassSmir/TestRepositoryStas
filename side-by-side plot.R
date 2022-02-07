a3d88149700d8463693a09647addb2468d1af053
StassSmir <97925680+StassSmir@users.noreply.github.com>
2022-02-07 01:43:00 -0500

    Add files via upload

---
+++ side-by-side plot.R
@@ -0,0 +1,23 @@
+Frequency <- c(0.6, 0.3, 0.4, 0.4, 0.2, 0.6, 0.3, 0.4, 0.9, 0.2)
+hospitaldata <- c("Frequency")
+boxplot(Frequency, names=hospitaldata, horizontal = TRUE)
+
+
+BP <- c(103, 87, 32, 42, 59, 109, 78, 205, 135, 176)
+hospitaldata <- c("BP")
+boxplot(BP, name=hospitaldata, horizontal = TRUE)
+
+First <- c(1, 1, 1, 1, 0, 0, 0, 0, NA, 1)
+hospitaldata <- c("First")
+boxplot(First, name=hospitaldata, horizontal = TRUE)
+boxplot(First, na.rn=TRUE)
+
+Second <- c(0, 0, 1, 1, 0, 0, 1, 1, 1, 1)
+hospitaldata <- c("Second")
+boxplot(Second, name=hospitaldata, horizontal = TRUE)
+
+FinalDecision <- c(0, 1, 0, 1, 0, 1, 0, 1, 1, 1)
+hospitaldata <- c("FinalDecision")
+boxplot(FinalDecision, name=hospitaldata, horizontal = TRUE)
+
+
